191a780cb8d413159e6359f9a60d297dea053748
Axel Santizo <65718815+Santizo00@users.noreply.github.com>
2025-02-24 22:13:26 -0600

    Update firebaseConfig.ts

--- Frontend/src/firebaseConfig.ts
+++ Frontend/src/firebaseConfig.ts
@@ -3,13 +3,7 @@ import { getAuth, GoogleAuthProvider, PhoneAuthProvider, multiFactor , sendSignI
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_CDNPI-0tR244to6NbYRKdLbAd3lESgY",
-  authDomain: "datadash-firebaselogin.firebaseapp.com",
-  projectId: "datadash-firebaselogin",
-  storageBucket: "datadash-firebaselogin.firebasestorage.app",
-  messagingSenderId: "760679953803",
-  appId: "1:760679953803:web:0031b0c9ad38867f58eb8e",
-  measurementId: "G-SN3F1MQQJJ"
+
 };
 
 const app = initializeApp(firebaseConfig);
